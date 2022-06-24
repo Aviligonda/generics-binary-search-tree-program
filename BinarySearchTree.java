0ba8fdf045716ae9d199090f0eefe4b80f47b7e9
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-24 13:42:36 +0530

    Added : Adding elements and print in different ways

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,7 +1,64 @@
 package com.binarysearchtree;
 
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
+    static Node root;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Binary Search Tree Program");
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+        System.out.println();
+        System.out.println("InOrder Print is left-->root-->right:");
+        binarySearchTree.inOrderPrint(root);
+        System.out.println();
+        System.out.println("PreOrder Print is root-->left-->right:");
+        binarySearchTree.preOrderPrint(root);
+        System.out.println();
+        System.out.println("PostOrder Print is left-->right-->root:");
+        binarySearchTree.postOrderPrint(root);
+
+    }
+
+    public Node<T> insertElements(Node<T> root, T key) {
+        if (root == null) {
+            root = new Node<>(key);
+        }
+        if (key.compareTo(root.key) < 0) {
+            root.left = insertElements(root.left, key);
+        } else if (key.compareTo(root.key) > 0) {
+            root.right = insertElements(root.right, key);
+        }
+        return root;
+    }
+
+    public void add(T key) {
+        root = insertElements(root, key);
+    }
+
+    public void inOrderPrint(Node<T> node) {
+        if (node != null) {
+            inOrderPrint(node.left);
+            System.out.print(node.key + " -->");
+            inOrderPrint(node.right);
+
+        }
+    }
+
+    public void preOrderPrint(Node<T> node) {
+        if (node != null) {
+            System.out.print(node.key + "-->");
+            preOrderPrint(node.left);
+            preOrderPrint(node.right);
+        }
+    }
+
+    public void postOrderPrint(Node<T> node) {
+        if (node != null) {
+            preOrderPrint(node.left);
+            preOrderPrint(node.right);
+            System.out.print(node.key + "-->");
+        }
     }
 }
---
+++ Node.java
@@ -0,0 +1,12 @@
+package com.binarysearchtree;
+
+public class Node<T extends Comparable<T>> {
+    T key;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T key) {
+        this.key = key;
+    }
+}
+
