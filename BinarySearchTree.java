20cdd08f1c8b8e5956bf671a10ac5f43b25fb77b
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-25 08:25:46 +0530

    Added :Another method in finding the node

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,7 +1,5 @@
 package com.binarysearchtree;
 
-import java.lang.annotation.ElementType;
-
 public class BinarySearchTree<T extends Comparable<T>> {
     static Node root;
 
@@ -12,19 +10,19 @@ public class BinarySearchTree<T extends Comparable<T>> {
         for (Integer i : elements) {
             binarySearchTree.add(i);
         }
+        System.out.println("In the Tree is  :" + binarySearchTree.searchNode(63));
+        binarySearchTree.findNode(56);
         System.out.println();
-        System.out.println("In the list :"+binarySearchTree.searchNode(63));
-        System.out.println();
-        System.out.println("Size of the elements is : " + binarySearchTree.size());
         System.out.println();
         System.out.println("InOrder Print is left-->root-->right:");
         binarySearchTree.inOrderPrint(root);
         System.out.println();
-        System.out.println("PreOrder Print is root-->left-->right:");
-        binarySearchTree.preOrderPrint(root);
-        System.out.println();
-        System.out.println("PostOrder Print is left-->right-->root:");
-        binarySearchTree.postOrderPrint(root);
+        //        System.out.println("Size of the elements is : " + binarySearchTree.size());
+//        System.out.println("PreOrder Print is root-->left-->right:");
+//        binarySearchTree.preOrderPrint(root);
+//        System.out.println();
+//        System.out.println("PostOrder Print is left-->right-->root:");
+//        binarySearchTree.postOrderPrint(root);
 
     }
 
@@ -47,54 +45,69 @@ public class BinarySearchTree<T extends Comparable<T>> {
     public void inOrderPrint(Node<T> node) {
         if (node != null) {
             inOrderPrint(node.left);
-            System.out.print(node.key + "-->");
+            System.out.print(node.key + " -->");
             inOrderPrint(node.right);
 
         }
     }
 
-    public void preOrderPrint(Node<T> node) {
-        if (node != null) {
-            System.out.print(node.key + "-->");
-            preOrderPrint(node.left);
-            preOrderPrint(node.right);
-        }
-    }
-
-    public void postOrderPrint(Node<T> node) {
-        if (node != null) {
-            preOrderPrint(node.left);
-            preOrderPrint(node.right);
-            System.out.print(node.key + "-->");
-        }
-    }
-
-    public int findingSize(Node<T> root) {
-        if (root == null) {
-            return 0;
-        } else {
-            return findingSize(root.right) + 1 + findingSize(root.left);
-        }
-    }
-
-    public int size() {
-        return findingSize(root);
-    }
-
     public boolean findingNode(Node<T> root, T key) {
         if (root == null) {
-            System.out.println("List is Empty");
             return false;
         } else if (key.compareTo(root.key) == 0) {
             return true;
+        } else {
+            if (key.compareTo(root.key) < 0) {
+                return findingNode(root.left, key);
+            } else {
+                return findingNode(root.right, key);
+            }
         }
-        return true;
-
     }
-
 
     public boolean searchNode(T key) {
-        System.out.println("Element :"+key);
+        System.out.println("Element :" + key);
         return findingNode(root, key);
     }
+// finding node in another way following this
+    public void findNode(T key) {
+        if (root == null) {
+            System.out.println("Tree is empty");
+        }
+        if (root.key.compareTo(key) == 0) {
+            System.out.println(key+" : Element found in the data : ");
+        } else {
+            System.out.println(key+" : Element not found in the data  :");
+        }
+    }
+
 }
+//
+//    public void preOrderPrint(Node<T> node) {
+//        if (node != null) {
+//            System.out.print(node.key + "-->");
+//            preOrderPrint(node.left);
+//            preOrderPrint(node.right);
+//        }
+//    }
+//
+//    public void postOrderPrint(Node<T> node) {
+//        if (node != null) {
+//            preOrderPrint(node.left);
+//            preOrderPrint(node.right);
+//            System.out.print(node.key + "-->");
+//        }
+//    }
+//
+//    public int findingSize(Node<T> root) {
+//        if (root == null) {
+//            return 0;
+//        } else {
+//            return findingSize(root.right) + 1 + findingSize(root.left);
+//        }
+//    }
+//
+//    public int size() {
+//        return findingSize(root);
+
+//    }
